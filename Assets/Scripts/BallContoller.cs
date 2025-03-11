fd413951d40bcddc00ea5894debe2e3691a4bc21
minu-96 <skydufan@naver.com>
2025-03-12 05:44:34 +0900

    샘플씬 생성, 스크립트 작성, 충돌처리 등

--- Assets/Scripts/BallContoller.cs
+++ Assets/Scripts/BallContoller.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
 
-public class BallContoller
+public class BallController : MonoBehaviour
 {
-    
+    public float speed = 10f; // 속도 조절
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
 }
